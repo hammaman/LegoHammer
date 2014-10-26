1cf3e883e1a554ef0d0a8672ae7616cf528d3e2f
hammaman <ahammacott@gmail.com>
2014-10-26 16:21:08 +0000

    Update for three sensors

--- 1. Line Sensors.py
+++ 1. Line Sensors.py
@@ -15,28 +15,49 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Define GPIO pins to use on the Pi
-GPIO_DETECT = 25
+GPIO_DETECTLFT = 25
+GPIO_DETECTMID = 7
+GPIO_DETECTRGH = 8
 
 print "Line detection"
 
 # Set pin as input
-GPIO.setup(GPIO_DETECT, GPIO.IN)
+GPIO.setup(GPIO_DETECTLFT, GPIO.IN)
+GPIO.setup(GPIO_DETECTMID, GPIO.IN)
+GPIO.setup(GPIO_DETECTRGH, GPIO.IN)
+
 
 try:
     # Repeat the next indented block forever
     while True:
 
         # If the sensor is HIGH (=1), the surface is detected as black
-		if GPIO.input(GPIO_DETECT) == 1:
-		    print "Detecting a black surface"
-		
-		# If not, the surface is detected as white
-		else:
-		    print "Detecting a white surface"
-			
-		# Wait a second, then do the same again
-		time.sleep(1)
-	
+        if GPIO.input(GPIO_DETECTMID) == 1:
+            print "MID: Detecting a black surface"
+        
+        # If not, the surface is detected as white
+        else:
+            print "MID: Detecting a white surface"
+
+        # If the sensor is HIGH (=1), the surface is detected as black
+        if GPIO.input(GPIO_DETECTLFT) == 1:
+            print "LEFT: Detecting a black surface"
+        
+        # If not, the surface is detected as white
+        else:
+            print "LEFT: Detecting a white surface"
+
+        # If the sensor is HIGH (=1), the surface is detected as black
+        if GPIO.input(GPIO_DETECTRGH) == 1:
+            print "RIGHT: Detecting a black surface"
+        
+        # If not, the surface is detected as white
+        else:
+            print "RIGHT: Detecting a white surface"
+            
+        # Wait a second, then do the same again
+        time.sleep(1)
+    
 # If you press Ctrl-C, this will generate a keyboard interrupt
 # and if so, clean up and stop
 except KeyboardInterrupt:
